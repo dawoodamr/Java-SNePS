a445cc25153de7c3f3723fb93baac7ffe0431596
amr.dawood1 <amr.dawood1@ce2d8127-8953-b8d5-caf8-800dbd40e5ae>
2010-03-23 15:49:04 +0000

    class entity updated.node added as a parameter as well as getter and setter

--- Java SNePS/src/sneps/Entity.java
+++ Java SNePS/src/sneps/Entity.java
@@ -3,9 +3,33 @@ package sneps;
 /**
  * The super class of all semantic classes of Nodes.
  * 
- * @author Amr Khaled Dawood 
+ * @author Amr Khaled Dawood
  */
 public class Entity
 {
 
+	/**
+	 * the node that contains this entity
+	 */
+	private Node node;
+
+	/**
+	 * @return the node that contains this entity
+	 */
+	public Node getNode()
+	{
+		return node;
+	}
+	
+	/**
+	 * this method is used after creating the entity in order to set the node that contains 
+	 * this entity
+	 * 
+	 * @param node the node that contains the entity
+	 */
+	public void setNode(Node node)
+	{
+		this.node = node;
+	}
+	
 }
